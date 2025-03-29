3d5e408f1246d853fb23b520f5dad85a1d90db4a
Paula Boutala <paulamaciel.3@gmail.com>
2025-03-29 14:50:38 -0300

    useState
    Aplicação do useState no componente Metodologia

--- my-app/src/components/Metodologia/index.js
+++ my-app/src/components/Metodologia/index.js
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import styles from './Metodologia.module.css';
 
 function Metodologia() {
+    const [passoAtivo, setPassoAtivo] = useState(1);
+
+    const textos = {
+        1: "Descrição do Passo 1",
+        2: "Descrição do Passo 2",
+        3: "Descrição do Passo 3"
+    };
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Nossa metodologia</h1>
             <div className={styles.content}>
                 <div className={styles.leftColumn}>
-                    <div className={styles.box}>Passo 1</div>
-                    <div className={styles.box}>Passo 2</div>
-                    <div className={styles.box}>Passo 3</div>
+                    {[1, 2, 3].map((num) => (
+                        <div
+                            key={num}
+                            className={`${styles.box} ${passoAtivo === num ? styles.bold : ""}`}
+                            onClick={() => setPassoAtivo(num)}
+                        >
+                            Passo {num}
+                        </div>
+                    ))}
                 </div>
-                <div className={styles.rightColumn}>Descrição detalhada</div>
+                <div className={styles.rightColumn}>
+                    {passoAtivo ? textos[passoAtivo] : "Descrição detalhada"}
+                </div>
             </div>
         </div>
     );
